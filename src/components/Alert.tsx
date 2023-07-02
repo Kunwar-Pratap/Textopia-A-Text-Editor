6fcb90bb88df11c1ce797371bc59e79e3bfabefc
Kunwar Pratap <kunwarkudos@gmail.com>
2023-07-02 22:23:27 +0530

    initial commit

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -10,11 +10,16 @@ type TProps = {
 
 const Alert: React.FC<TProps> = ({ type, msg }) => {
 
+    const capitalize = (word: string) => {
+        const lower = word.toLowerCase();
+        return lower.charAt(0).toUpperCase() + lower.slice(1);
+    }
+
     const { alert } = useContext(AlertContext);
 
     return (
         alert && <div className="h-12 px-3 text-base text-emerald-800 rounded-lg bg-emerald-200 dark:bg-violet-950 dark:text-violet-400 relative flex items-center" role="alert">
-            <strong className="pr-2">{alert.type}&nbsp;:</strong> {alert.msg}
+            <strong className="pr-2">{capitalize(alert.type)}&nbsp;:</strong> {capitalize(alert.msg)}
         </div>
     )
 }
